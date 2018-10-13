05af7d5e5c75171f34c52e03c9ba922d04ab6d02
Deborah Castiglioni <debjgorton@gmail.com>
2018-10-13 03:05:31 -0400

    Fix 400 error

--- src/FourSquare.js
+++ src/FourSquare.js
@@ -1,31 +1,30 @@
-/* This method of adding the foursqaure API is from Forrest Walker's Connecting Foursquare video */
+/* This method of adding the foursqaure API is from Forrest Walker's Connecting
+Foursquare video */
 
 //Use static methods on the class.
+
 class Foursquare {
   static baseURL() {
-    return "https://api.foursquare.com/v2/";
+    return "https://api.foursquare.com/v2";
   }
-
-  static authentication() {
-    const apiKey = {
+  static authentic() {
+    const fsKeys = {
       client_id: "YXHLWLDBYKSGUSIOWO5FWJVTFRQW1OAOMAFWV4SNYLDX5UOW",
       client_secret: "CT5CNTHXA54TBPTHHR4GGTNATFYZZDIJD3LTSYP3CJO5GV0X",
-      v: "20181012"
+      v: "20181013"
     };
 
-    return Object.keys(apiKey)
-      .map(key => `${apiKey}=${apiKey[key]}`)
+    return Object.keys(fsKeys)
+      .map(key => `${key}=${fsKeys[key]}`)
       .join("&");
   }
-
   static urlBuilder(params) {
     if (!params) {
       return "";
-    } else {
-      return Object.keys(params)
-        .map(key => `${key}=${params[key]}`)
-        .join("&");
     }
+    return Object.keys(params)
+      .map(key => `${key}=${params[key]}`)
+      .join("&");
   }
 
   static headers() {
@@ -34,30 +33,30 @@ class Foursquare {
     };
   }
 
-  static getFetch(endpoint, method, params) {
+  static getFetch(endPoint, method, params) {
     let getData = {
       method,
       headers: Foursquare.headers()
     };
+
     return fetch(
-      `${Foursquare.baseURL()}${endpoint}?${Foursquare.authentication()}&${Foursquare.urlBuilder(
+      `${Foursquare.baseURL()}${endPoint}?${Foursquare.authentic()}&${Foursquare.urlBuilder(
         params
       )}`,
       getData
     ).then(get => get.json());
   }
 }
-
-export default class FoursquareAPI {
+export default class SquareAPI {
   static search(params) {
-    return Foursquare.getFetch("venues/search", "GET", params);
+    return Foursquare.getFetch(`/venues/search`, "GET", params);
   }
 
   static venueInfo(VENUE_ID) {
-    return Foursquare.getFetch(`venues/${VENUE_ID}`, "GET");
+    return Foursquare.getFetch(`/venues/${VENUE_ID}`, "GET");
   }
 
-  static venuePhoto(VENUE_ID) {
-    return Foursquare.getFetch(`venues/${VENUE_ID}/photos`, "GET");
+  static venuePhotos(VENUE_ID) {
+    return Foursquare.getFetch(`/venues/${VENUE_ID}/photos`, "GET");
   }
 }
