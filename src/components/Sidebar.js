e936efbfbea3c157abaac058d11f9d4080b25793
Deborah Castiglioni <debjgorton@gmail.com>
2018-10-20 03:33:48 -0400

    Add input for search

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -5,6 +5,7 @@ export default class Sidebar extends Component {
     render() {
         return (
             <div className="sidebar">
+            <input type={"search"} id={"search"} placeholder={"Favorite volcano"} />
             <Volcanoes />
             </div>
         )
